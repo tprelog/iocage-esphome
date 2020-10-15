2baf6366e80648f3075e07dcaa03f0b6f0ccec7b
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2020-10-15 00:01:23 -0400

    12.1-RELEASE

--- pre_update.sh
+++ pre_update.sh
@@ -3,4 +3,4 @@
 ## Generate a list of manually installed packages
 ## This list will used to (hopefully) ensure that any user installed FreeBSD packages,
 ##  not included in the plugin manifest, will be reinstalled after a plugin update
-pkg query -e '%a = 0' %n | sort > /tmp/pkglist
+pkg prime-list > /tmp/pkglist
